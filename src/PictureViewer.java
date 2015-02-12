45824d71a20efe153f4cd44cb759ea70965e8d16
garrysSon <john.garrison@ou.edu>
2015-02-11 18:06:08 -0600

    Initial commit of project

---
+++ src/PictureViewer.java
@@ -0,0 +1,20 @@
+import java.awt.event.*;
+import javax.swing.JFrame;
+
+
+/**
+ * A picture viewer.
+ * 
+ * @author BruceLee
+ */
+@SuppressWarnings("serial")
+public class PictureViewer extends JFrame implements ActionListener
+{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
